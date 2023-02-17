da88c54a6fd64d44970b48138cbd9c6527b7a1e1
Coleman Dole <mrnameloc@gmail.com>
2023-02-17 14:35:34 -0600

    wip

--- aux_scripts/scraper.ps1
+++ aux_scripts/scraper.ps1
@@ -1,10 +1,5 @@
-$deities = Invoke-WebRequest -Method Get -Uri "https://aonsrd.com/Deities.aspx?ItemName=All"
-Write-Output "{
-    `"Deity`": ["
-($deities.content | Select-String 'href\="Deities.aspx\?ItemName\=(.*?)">' -AllMatches).Matches.Groups | Where-Object Name -eq 1 | Select-Object @{e="Value";l="Deity"} -Unique | ForEach-Object {
-    if ($_.Deity -ne "All") {
-        Write-Output ","
-    }
-    Write-Output "`"$($_.Deity)`""
-}
-Write-Output "]}"
+# $deities = Invoke-WebRequest -Method Get -Uri "https://aonsrd.com/Deities.aspx?ItemName=All"
+# ($deities.content | Select-String 'href\="Deities.aspx\?ItemName\=(.*?)">' -AllMatches).Matches.Groups | Where-Object Name -eq 1 | Select-Object @{e="Value";l="Deity"} -Unique
+
+$races = Invoke-WebRequest -Method Get -Uri "https://aonsrd.com/Races.aspx?ItemName=All"
+($races.content | Select-String 'href\="Races.aspx\?ItemName\=(.*?)">' -AllMatches).Matches.Groups | Where-Object Name -eq 1 | Select-Object @{e="Value";l="Race"} -Unique | ConvertTo-Csv #|Out-File "C:\Scripts\races.csv"
