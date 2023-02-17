469ed0cb8f4f21de731efad8052f6bebde754fb6
Coleman Dole <mrnameloc@gmail.com>
2023-02-17 15:20:20 -0600

    added alignment selection

--- aux_scripts/scraper.ps1
+++ aux_scripts/scraper.ps1
@@ -1,5 +1,8 @@
 # $deities = Invoke-WebRequest -Method Get -Uri "https://aonsrd.com/Deities.aspx?ItemName=All"
 # ($deities.content | Select-String 'href\="Deities.aspx\?ItemName\=(.*?)">' -AllMatches).Matches.Groups | Where-Object Name -eq 1 | Select-Object @{e="Value";l="Deity"} -Unique
 
-$races = Invoke-WebRequest -Method Get -Uri "https://aonsrd.com/Races.aspx?ItemName=All"
-($races.content | Select-String 'href\="Races.aspx\?ItemName\=(.*?)">' -AllMatches).Matches.Groups | Where-Object Name -eq 1 | Select-Object @{e="Value";l="Race"} -Unique | ConvertTo-Csv #|Out-File "C:\Scripts\races.csv"
+# $races = Invoke-WebRequest -Method Get -Uri "https://aonsrd.com/Races.aspx?ItemName=All"
+# ($races.content | Select-String 'href\="Races.aspx\?ItemName\=(.*?)">' -AllMatches).Matches.Groups | Where-Object Name -eq 1 | Select-Object @{e="Value";l="Race"} -Unique | ConvertTo-Csv
+
+$alignment = Invoke-WebRequest -Method Get -Uri "https://aonsrd.com/Rules.aspx?ID=1772"
+($alignment.content | Select-String '\<a href\="Rules\.aspx\?ID\=\d+"\>(.*?)\<\/a\>' -AllMatches).Matches.Groups | Where-Object Name -eq 1 | Select-Object @{e="Value";l="Alignment"} -Unique | ConvertTo-Csv
