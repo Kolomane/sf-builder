2a68ef3aad88ea1e5f245ddd302e0d99f3540f25
Coleman Dole <mrnameloc@gmail.com>
2023-02-25 13:51:53 -0600

    Updates as of 02/25/2023

--- aux_scripts/scraper.ps1
+++ aux_scripts/scraper.ps1
@@ -1,8 +1,54 @@
 # $deities = Invoke-WebRequest -Method Get -Uri "https://aonsrd.com/Deities.aspx?ItemName=All"
-# ($deities.content | Select-String 'href\="Deities.aspx\?ItemName\=(.*?)">' -AllMatches).Matches.Groups | Where-Object Name -eq 1 | Select-Object @{e="Value";l="Deity"} -Unique
+# $deityList = ($deities.content | Select-String 'href\="Deities.aspx\?ItemName\=(.*?)">' -AllMatches).Matches.Groups | Where-Object Name -eq 1 | Select-Object @{e="Value";l="Deity"}
+
+# $deityList = ($deities.content | Select-String '\<tr( style\=\"background\-color\:\#CCCCCC\;\")?\>([\v\r\f\s]*.*[\v\r\f\s]*)<\/tr>' -AllMatches).Matches
+
+# .?\<td\>\<a href\="Deities\.aspx\?ItemName\=.*?"\>(.*?)\<\/a\>\<\/td\>\<td\>(.*?)\<\/td\>\<td\>(.*?)\<\/td\>\<td\>(.*?)\<\/td\>.?\
+# <tr>
+# <td><a href="Deities.aspx?ItemName=Hylax">Hylax</a></td><td>Core Deities</td><td>LG</td><td>diplomacy, first contact, friendship, peace</td>
+# </tr>
 
 # $races = Invoke-WebRequest -Method Get -Uri "https://aonsrd.com/Races.aspx?ItemName=All"
 # ($races.content | Select-String 'href\="Races.aspx\?ItemName\=(.*?)">' -AllMatches).Matches.Groups | Where-Object Name -eq 1 | Select-Object @{e="Value";l="Race"} -Unique | ConvertTo-Csv
 
-$alignment = Invoke-WebRequest -Method Get -Uri "https://aonsrd.com/Rules.aspx?ID=1772"
-($alignment.content | Select-String '\<a href\="Rules\.aspx\?ID\=\d+"\>(.*?)\<\/a\>' -AllMatches).Matches.Groups | Where-Object Name -eq 1 | Select-Object @{e="Value";l="Alignment"} -Unique | ConvertTo-Csv
+# $alignment = Invoke-WebRequest -Method Get -Uri "https://aonsrd.com/Rules.aspx?ID=1772"
+# ($alignment.content | Select-String '\<a href\="Rules\.aspx\?ID\=\d+"\>(.*?)\<\/a\>' -AllMatches).Matches.Groups | Where-Object Name -eq 1 | Select-Object @{e="Value";l="Alignment"} -Unique | ConvertTo-Csv
+
+$conditions = Invoke-WebRequest -Method Get -Uri "https://aonsrd.com/Rules.aspx?ID=165"
+$conditionsList = ($conditions.content | Select-String '2 class="title">(?<Title>.*?)<\/h2>(.|\n)*?<i>(?<Source>.*?)<\/i>(.|\n)*?<br \/>(?<Description>.*?)(<h|<\/span)' -AllMatches).Matches
+
+$conditionsListJSON = @"
+{
+    "Conditions": {}
+}
+"@ | ConvertFrom-Json
+
+$conditionsList.Groups | Where-Object {($_.Name -eq "Description") -or ($_.Name -eq "Source") -or ($_.Name -eq "Title")} | Select Value | Foreach-Object {
+    $tempTitle = ""
+    $tempSource = ""
+    switch -regex ("$($_.Name)") {
+        "Title" {$tempTitle = "$($_.Value)"}
+        "Source" {$tempSource = "$($_.Value)"}
+        "Description" {
+            $tempJSON = @"
+{
+    "Source":$tempSource,
+    "Description":"$($_.Value)"
+}
+"@
+            $conditionsListJSON.Conditions | add-member -Name "$tempTitle" -value (Convertfrom-Json $tempJSON) -MemberType NoteProperty
+        }
+    }
+}
+
+# "@ | ConvertFrom-Json
+# $conditionsList | ForEach-Object {
+#     # Write-Output "$($_.Groups[1]), $($_.Groups[2]), $($_.Groups[3])"
+#     $tempJSON = @"
+# {
+#     "Source":"$($_.Groups[3])",
+#     "Description":"$($_.Groups[5])"
+# }
+# "@
+#     $conditionsListJSON.Conditions | add-member -Name "$($_.Groups[1])" -value (Convertfrom-Json $tempJSON) -MemberType NoteProperty
+# }
