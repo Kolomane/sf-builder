d6a45a75fd41687e983b691ef1a3c49bc02d3ca9
Coleman Dole <mrnameloc@gmail.com>
2023-02-25 21:28:33 -0600

    updates from 02/25/2023

--- aux_scripts/scraper.ps1
+++ aux_scripts/scraper.ps1
@@ -15,17 +15,16 @@ cls
 # $alignment = Invoke-WebRequest -Method Get -Uri "https://aonsrd.com/Rules.aspx?ID=1772"
 # ($alignment.content | Select-String '\<a href\="Rules\.aspx\?ID\=\d+"\>(.*?)\<\/a\>' -AllMatches).Matches.Groups | Where-Object Name -eq 1 | Select-Object @{e="Value";l="Alignment"} -Unique | ConvertTo-Csv
 # 
-# $conditions = Invoke-WebRequest -Method Get -Uri "https://aonsrd.com/Rules.aspx?ID=165"
 
+#########################################
+# CONDITIONS SCRAPER
+$conditions = Invoke-WebRequest -Method Get -Uri "https://aonsrd.com/Rules.aspx?ID=165"
 $conditionsList = ($conditions.content | Select-String '2 class="title">(?<Title>.*?)<\/h2>(.|\n)*?<i>(?<Source>.*?)<\/i>(.|\n)*?<br \/>(?<Description>.*?)(<h|<\/span)' -AllMatches).Matches
-
 $conditionsListJSON = @"
 {
     "Conditions": {}
 }
 "@ | ConvertFrom-Json
-# $tempTitle = ""
-# $tempSource = ""
 $conditionsList.Groups | Where-Object {($_.Name -eq "Description") -or ($_.Name -eq "Source") -or ($_.Name -eq "Title")} | Foreach-Object {
     if ($_.Name -eq "Title") {
         $tempTitle = "$($_.Value)"
@@ -44,15 +43,3 @@ $conditionsList.Groups | Where-Object {($_.Name -eq "Description") -or ($_.Name 
     }
 }
 $conditionsListJSON | ConvertTo-Json | Out-File "./json/conditions.json"
-
-# "@ | ConvertFrom-Json
-# $conditionsList | ForEach-Object {
-#     # Write-Output "$($_.Groups[1]), $($_.Groups[2]), $($_.Groups[3])"
-#     $tempJSON = @"
-# {
-#     "Source":"$($_.Groups[3])",
-#     "Description":"$($_.Groups[5])"
-# }
-# "@
-#     $conditionsListJSON.Conditions | add-member -Name "$($_.Groups[1])" -value (Convertfrom-Json $tempJSON) -MemberType NoteProperty
-# }
